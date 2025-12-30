d9850e3ab1998b5ccfdccdc968c5c68d80ca2611
K4TaNÆ <omegavinerun@gmail.com>
2025-12-30 13:04:18 +0200

    Update asd.lua

--- asd.lua
+++ asd.lua
@@ -147,7 +147,7 @@ Queue.new = function()
 				-- pcall(function()
 				-- 	local failed = self:dequeue(true)
 				-- 	self:enqueue(failed)
-				self:enqueue(self.data[self.__head])
+					self:dequeue(self.data[self.__head])
 				-- end)
 			end
 			while self.__head <= self.__tail do
