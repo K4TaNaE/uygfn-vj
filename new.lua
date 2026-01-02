ec83f3914e73ed4fac49765cfc932eeaee14e7ca
K4TaNÆ <omegavinerun@gmail.com>
2026-01-02 18:40:05 +0200

    Update new.lua

--- new.lua
+++ new.lua
@@ -468,7 +468,7 @@ local function get_equiped_pet_ailments() -- optimized
 	local pet = ClientData.get("pet_char_wrappers")[1]
 	if pet then
 		local path = ClientData.get("ailments_manager")["ailments"][pet.pet_unique]
-		if not path then repeat print("waiting for path") task.wait(10) until ClientData.get("ailments_manager")["ailments"][pet.pet_unique] end
+		if not path then return nil end
 		for k,_ in ClientData.get("ailments_manager")["ailments"][pet.pet_unique] do
 			ailments[k] = true
 		end
@@ -1395,6 +1395,12 @@ local function init_autofarm() -- optimized
 				break
 			end
 			local eqpetailms = get_equiped_pet_ailments()
+			if not eqpetailms then
+				repeat 
+					eqpetailms = get_equiped_pet_ailments()
+					task.wait(10)
+				until eqpetailms
+			end
 			for k,_ in eqpetailms do 
 				if StateDB.active_ailments[k] then continue end
 				if pet_ailments[k] then
