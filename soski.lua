ceedebc6a082c64894dcc16605b7b5a82263ab33
K4TaNÆ <omegavinerun@gmail.com>
2026-01-02 17:25:36 +0200

    Update soski.lua

--- soski.lua
+++ soski.lua
@@ -142,10 +142,10 @@ Queue.new = function()
 			self.running = true
 
 			while not self:empty() do
-				local task = self._data[self.__head]
+				local dtask = self._data[self.__head]
 
-				local name = task[1]
-				local callback = task[2]
+				local name = dtask[1]
+				local callback = dtask[2]
 
 				local ok, err = xpcall(callback, debug.traceback)
 				self:dequeue(true)
@@ -154,6 +154,7 @@ Queue.new = function()
 					print("Task failed:", err)
 					local spl = name:split(": ")
 					if spl[1]:match("ailment pet") then
+						print(spl[2])
 						StateDB.active_ailments[spl[2]] = nil
 					elseif spl[1]:match("ailment baby") then
 						StateDB.active_ailments_baby[spl[2]] = nil
@@ -424,6 +425,13 @@ local function get_equiped_pet() -- not optimzed
  	return data
 end
 
+local function cur_unique() 
+	local path = ClientData.get("pet_char_wrappers")[1]
+	if path then
+		return path.pet_unique
+	end
+end
+
 local function get_owned_pets() -- optimized
 	local data = {}
 	for _,v in ClientData.get("inventory").pets do
@@ -461,8 +469,8 @@ local function get_equiped_pet_ailments() -- optimized
 	local pet = ClientData.get("pet_char_wrappers")[1]
 	if pet then
 		local path = ClientData.get("ailments_manager")["ailments"][pet.pet_unique]
-		if not path then repeat print("still waiting ") task.wait(.5) until path end
-		for k,_ in path do
+		if not path then repeat print("waiting for path") task.wait(10) until ClientData.get("ailments_manager")["ailments"][pet.pet_unique] end
+		for k,_ in ClientData.get("ailments_manager")["ailments"][pet.pet_unique] do
 			ailments[k] = true
 		end
 	end
@@ -568,12 +576,12 @@ local function count(t)
 end
 
 local function gotovec(x:number, y:number, z:number) -- optimized
-	local pet = get_equiped_pet()
-	if pet then
-		PetActions.pick_up(pet.model)
+	local pet = actual_pet
+	if pet.unique then
+		PetActions.pick_up(pet.wrapper)
 		task.wait(.2)
 		LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(x,y,z)
-		task.wait(.1)
+		task.wait(.2)
 		API["AdoptAPI/EjectBaby"]:FireServer(pet.model)
 	else
 		LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(x,y,z)
@@ -656,7 +664,7 @@ local function enstat(xp, friendship, money, ailment)  -- optimized
 	end
 
 	if _G.InternalConfig.AutoFarmFilter.PotionFarm then
-		if friendship < get_equiped_pet().friendship then
+		if friendship < ClientData.get("inventory").pets[actual_pet.unique].properties.friendship_level then
 			farmed.friendship_levels += 1
 			farmed.potions += 1
 			table.clear(StateDB.active_ailments)
@@ -666,7 +674,7 @@ local function enstat(xp, friendship, money, ailment)  -- optimized
 			StateDB.active_ailments[ailment] = nil
 		end
 	else 
-		if xp >= xp_thresholds[get_equiped_pet().rarity][6] then
+		if xp >= xp_thresholds[actual_pet.rarity][6] then
 			farmed.pets_fullgrown += 1
 			table.insert(total_fullgrowned, actual_pet.unique)
 			update_gui("fullgrown", farmed.pets_fullgrown)
@@ -960,6 +968,7 @@ local pet_ailments = {
 			queue:destroy_linked("ailment pet")
 			actual_pet.unique = nil
 			table.clear(StateDB.active_ailments)
+			print("error to dirty")
 			return 
 		end
 		local cdata = ClientData.get("inventory").pets[actual_pet.unique]
@@ -982,6 +991,7 @@ local pet_ailments = {
         until not has_ailment("dirty") or os.clock() > deadline
         if os.clock() > deadline then error("Out of limits") end
 		enstat(xp, friendship, money, "dirty")  
+		print("dirty completed")
 	end,
 	["walk"] = function() 
 		local pet = ClientData.get("pet_char_wrappers")[1]
@@ -1070,6 +1080,7 @@ local pet_ailments = {
 			1,
 			k
 		)
+		task.wait(.4)
 		end				
 	end,
 	["pizza_party"] = function() 
@@ -1379,7 +1390,11 @@ local function init_autofarm() -- optimized
 		actual_pet.wrapper = curpet.wrapper
 		actual_pet.rarity = curpet.rarity
 
-		while actual_pet.unique do
+		while true do
+			if actual_pet.unique ~= cur_unique() then
+				actual_pet.unique = nil
+				break
+			end
 			local eqpetailms = get_equiped_pet_ailments()
 			for k,_ in eqpetailms do 
 				if StateDB.active_ailments[k] then continue end
@@ -1388,7 +1403,7 @@ local function init_autofarm() -- optimized
 					StateDB.active_ailments[k] = true
 				end
 			end
-			task.wait(15)
+			task.wait(20)
 		end
 	end
 end
@@ -1728,17 +1743,21 @@ end)
 		if (Config.FarmPriority):lower() == "eggs" or (Config.FarmPriority):lower() == "pets" then
 			_G.InternalConfig.FarmPriority = Config.FarmPriority
 			if type(Config.AutoFarmFilter.PetsToExclude) == "table" then -- AutoFarmFilter / PetsToExclude
-				if not (#Config.AutoFarmFilter.PetsToExclude == 1 and Config.AutoFarmFilter.PetsToExclude[1]:match("^%s*$")) then
-					local list = {}
-					for _,v in Config.AutoFarmFilter.PetsToExclude do
-						if check_remote_existance("pets", v) then
-							list[v] = true
-						else
-							colorprint({markup.ERROR}, `[-] Wrong "{v}" remote name `)
+				if not #Config.AutoFarmFilter.PetsToExclude == 0 then
+					if Config.AutoFarmFilter.PetsToExclude[1]:match("^%s*$") then
+						local list = {}
+						for _,v in Config.AutoFarmFilter.PetsToExclude do
+							if check_remote_existance("pets", v) then
+								list[v] = true
+							else
+								colorprint({markup.ERROR}, `[-] Wrong "{v}" remote name `)
+							end
 						end
-					end
-					if count(list) > 0 then
-						_G.InternalConfig.AutoFarmFilter.PetsToExclude = list
+						if count(list) > 0 then
+							_G.InternalConfig.AutoFarmFilter.PetsToExclude = list
+						else
+							_G.InternalConfig.AutoFarmFilter.PetsToExclude = {}
+						end
 					else
 						_G.InternalConfig.AutoFarmFilter.PetsToExclude = {}
 					end
@@ -2029,13 +2048,7 @@ end)()
 
 -- launch screen
 ;(function() -- optmized
-	while true do
-		if not UIManager.is_visible("NewsApp") then
-			task.wait(1)
-		else
-			break
-		end
-	end
+	if not UIManager.is_visible("MainMapApp") and not UIManager.is_visible("NewsApp") then return end
 	API["TeamAPI/ChooseTeam"]:InvokeServer("Parents", {source_for_logging="intro_sequence"})
 	task.wait(1)
 	UIManager.set_app_visibility("MainMenuApp", false)
