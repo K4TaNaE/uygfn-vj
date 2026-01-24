b85dc2e09dcbd9598d721b0d10b8a01092c72825
K4TaNÆ <omegavinerun@gmail.com>
2026-01-24 16:01:00 +0200

    Modify garbage collection in watchdog function
    Commented out garbage collection step in watchdog function.

--- asd.lua
+++ asd.lua
@@ -2300,8 +2300,8 @@ end
 
 Scheduler:add("gc", 300, function() -- watchdog
 	print('watchdog working')
-	collectgarbage("step", 260)
-end, false, true) 
+	-- collectgarbage("step", 260)
+end, false, false) 
 
 _G.CONNECTIONS.AntiAFK = LocalPlayer.Idled:Connect(function() -- anti afk
 	Scheduler:add("AntiAFK", 0, function() 
